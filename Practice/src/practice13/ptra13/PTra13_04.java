e4ab3c6a4c7f1f6161211aa0988e169e78ab5a3d
KagaRaymond <45344998+KagaRaymond@users.noreply.github.com>
2018-12-04 16:22:42 +0700

    13-3

--- Practice/src/practice13/ptra13/PTra13_04.java
+++ Practice/src/practice13/ptra13/PTra13_04.java
@@ -6,6 +6,9 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Hero;
+import practice13.common.Slime;
+
 public class PTra13_04 {
 
 	/*
@@ -15,6 +18,11 @@ public class PTra13_04 {
 	public static void main(String[] args) {
 
 		// ★ HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
+		Hero You = new Hero();
+		Slime Enemy = new Slime();
+
+		You.setName("勇者");
+		Enemy.setName("スライム");
 
 
 		/*
@@ -24,8 +32,22 @@ public class PTra13_04 {
 		 * 上記を繰り返し行います
 		 */
 
+		while (true) {
+			if(Enemy.damage(You.attack())) {
+				// slimedead
+				System.out.println(You + "は" + Enemy  + "との戦闘に勝利した");
+				break;
+			}
+
+			if(You.damage(Enemy.attack())) {
+				// herodead
+				System.out.println( Enemy + "は" + You  + "との戦闘に勝利した");
+				break;
+			}
+			return;
+		}
+
 
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
-
 	}
 }
